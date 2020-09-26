ac852308d066e39cd0a3dbcea337e3691e7137bc
Poovarasu Sekar <spoovarasu1997@gmail.com>
2020-09-26 08:37:09 +0530

    Initial Commit

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * UrlHum (https://urlhum.com)
+ *
+ * @link      https://github.com/urlhum/UrlHum
+ * @copyright Copyright (c) 2019 Christian la Forgia
+ * @license   https://github.com/urlhum/UrlHum/blob/master/LICENSE.md (MIT License)
+ */
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['prefix' => 'v1',  'middleware' => 'auth:api'], function () {
+    Route::apiResource('url', 'api\UrlController');
+});
